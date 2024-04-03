3673e12b648facf17d9ba452bc3e935c818d5e60
usman1170 <usman.zrr@gmail.com>
2024-04-03 23:38:23 +0500

    code update

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -34,66 +34,30 @@ class BackPopButton extends StatelessWidget {
   }
 }
 
-class Button extends StatelessWidget {
-  Button({
-    required this.text,
-    required this.color,
-    required this.ontap,
-    super.key,
-  });
-  final VoidCallback ontap;
-  String text;
-  Color color;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: ontap,
-      child: Container(
-        height: 55,
-        width: MediaQuery.of(context).size.width - 20,
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-                color: Colors.white,
-                fontSize: 17,
-                letterSpacing: 0.7,
-                fontWeight: FontWeight.w400),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class NewButton extends StatelessWidget {
+//   const NewButton({super.key, required this.ontap, required this.title});
+//   final VoidCallback ontap;
+//   final String title;
 
-class NewButton extends StatelessWidget {
-  const NewButton({super.key, required this.ontap, required this.title});
-  final VoidCallback ontap;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        shape: const StadiumBorder(),
-        backgroundColor: Colors.amber,
-        elevation: 8,
-        shadowColor: Colors.amber,
-        minimumSize: const Size.fromHeight(55),
-      ),
-      onPressed: ontap,
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       style: ElevatedButton.styleFrom(
+//         shape: const StadiumBorder(),
+//         backgroundColor: Colors.amber,
+//         elevation: 8,
+//         shadowColor: Colors.amber,
+//         minimumSize: const Size.fromHeight(55),
+//       ),
+//       onPressed: ontap,
+//       child: Text(
+//         title,
+//         style: const TextStyle(
+//           fontSize: 18,
+//           color: Colors.white,
+//           fontWeight: FontWeight.w700,
+//         ),
+//       ),
+//     );
+//   }
+// }
